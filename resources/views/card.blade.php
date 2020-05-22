f4b36046a0b1e2b303704dc2d71e25c0ce1cb288
Ivan Pushingit <ivango23456@gmail.com>
2020-05-22 23:20:14 +0400

    Pivot table. Также считает сумму товара от количества его и общую сумму заказа.

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -5,7 +5,7 @@
             <h3>{{$product->name}}</h3>
             <p>{{$product->price}} руб.</p>
             <p>
-                <form action="{{ route('basket-add', $product->id) }}" method="POST">
+                <form action="{{ route('basket-add', $product) }}" method="POST">
                 <button type="submit" class="btn btn-primary" role="button">В корзину</button>
                 <a href="{{ route('product', [$product->category->code, $product->code]) }}" class="btn btn-default"
                    role="button">Подробнее</a>
