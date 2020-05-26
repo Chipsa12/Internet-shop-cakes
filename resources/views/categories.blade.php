b951ad7ec56edd35f8b5b3248ff0c383de88142f
Ivan Pushingit <ivango23456@gmail.com>
2020-05-26 19:46:41 +0400

    Поправил верстку. Рестрация и Авторизация стала по центру.

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -6,7 +6,7 @@
     @foreach($categories as $category)
         <div class="panel">
             <a href="{{ route('category', $category->code) }}">
-                <img src="http://laravel-diplom-1.rdavydov.ru/storage/categories/mobile.jpg">
+                <img width="50px" src="https://big-cake-house.ru/assets/cache/products/410/tortik-s-yagodnyim-dekorom_0x1080_9f9.jpg">
                 <h2>{{ $category->name }}</h2>
             </a>
             <p>
