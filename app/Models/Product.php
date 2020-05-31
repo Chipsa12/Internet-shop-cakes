b47f00c72852d221d3d652b372fd2f976f419987
Ivan Pushingit <ivango23456@gmail.com>
2020-05-31 21:11:02 +0400

    Кол-во товара, Soft Delete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'code', 'price', 'category_id', 'description', 'image', 'hit', 'new', 'recommend'];
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'code', 'price', 'category_id', 'description', 'image', 'hit', 'new', 'recommend', 'count'];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function isAvailable() {
+        return  !$this->trashed() && $this->count > 0;
     }
 
     public function getPriceForCount() {
@@ -18,6 +25,10 @@ class Product extends Model
             return $this->pivot->count * $this->price;
         }
         return $this->price;
+    }
+
+    public function scopeByCode($query, $code) {
+        return $query->where('code', $code);
     }
 
     public function scopeHit($query) {
