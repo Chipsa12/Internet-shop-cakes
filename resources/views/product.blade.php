5801f7f9c4eb4c6e9513ef4faa24df2b623d3d24
Ivan Pushingit <ivango23456@gmail.com>
2020-06-06 22:06:20 +0400

    Исправления верстки и название у категории

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,10 +3,12 @@
 @section('title', 'Товар')
 
 @section('content')
-    <h1>{{ $product->name }}</h1>
-    <h2>{{ $product->category->name }}</h2>
-    <p>Цена: <b>{{ $product->price }} руб.</b></p>
-    <img src="{{ Storage::url($product->image) }}">
+<div class="my-menu-1">
+<h1>{{ $product->name }}</h1>
+<img width="400px" src="{{ Storage::url($product->image) }}">
+</div>
+<div class="my-menu">
+    <h2><p>Цена: <b>{{ $product->price }} руб.</b></p></h2>
     <p>{{ $product->description }}</p>
     @if($product->isAvailable())
         <form action="{{ route('basket-add', $product) }}" method="POST">
@@ -22,10 +24,13 @@
                 {!! $errors->get('email')[0] !!}
             @endif
         </div>
-        <form method="POST" action="{{ route('subscription', $product) }}">
-            @csrf
-            <input type="text" name="email" placeholder="Email"></input>
-            <button type="submit">Отправить</button>
+        <form method="POST" action="{{ route('subscription', $product) }}" class="my-menu-2">
+            <div class="input-group">
+                <input type="text" name="email" placeholder="Email"></input>
+                <button class="btn btn-outline-secondary" type="submit">Отправить</button>
+            </div>
+        @csrf
         </form>
     @endif
+</div>
 @endsection
